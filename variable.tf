392d9a8e087e31f67051129c8bfe7977aaeefb3c
rofaulo2023 <133694130+rofaulo2023@users.noreply.github.com>
2023-06-01 16:02:50 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,12 @@ variable "ResourceGroup" {
     type        = string
     default     = "rg-connectivity-network-001"   
 }
+variable "Location" {
+    type        = string
+    default     = "WestEurope"  
+}
+
+variable "ResourceGroup" {
+    type        = string
+    default     = "rg-loganalytics-001"   
+}
