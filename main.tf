ed737cd4576c37dd60facdc8949dac80497cdcbf
Rofaulo Elopre <rofaulo@iris.net>
2023-06-29 16:51:45 +0100

    main update

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
 terraform {
-  required_version = ">= 1.1.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
   cloud {
     organization = "poc-iris-infrastructure"
+
     workspaces {
-    name = "rc-test"
+      name = "rc-test"
     }
   }
 }
