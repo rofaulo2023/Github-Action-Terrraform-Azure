ca577c185d19b5322924f8acd26923ba0ad8d481
Rofaulo Elopre <rofaulo@iris.net>
2023-06-29 16:40:24 +0100

    main1

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
-resource "azurerm_resource_group" "resourcegroups" {
-    name        = var.ResourceGroup
-    location    = var.Location
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "poc-iris-infrastructure"
+    workspaces {
+    name = "rc-test"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
